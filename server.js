b8b15ea243b5c87e3fc89feac64801897696e1ba
Tigro <easterlingme95@gmail.com>
2022-12-22 15:01:36 -0600

    fixed port issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const {v4: uuidv4} = require('uuid');
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 
