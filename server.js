483793b20a9d076fcc676ef99e0333b296aad2b3
Tigro <easterlingme95@gmail.com>
2022-12-22 14:30:25 -0600

    added basic server needs and get routes

--- server.js
+++ server.js
@@ -6,3 +6,21 @@ const {v4: uuidv4} = require('uuid');
 const PORT = 3002;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile('./db/db.json', "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({message: "Error getting notes."})
+    }
+    res.send(data);
+  });
+});
